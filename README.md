467e679f86f23755aa14c0ef38f2596ddaa043ca
shawn-skt <2857043555@qq.com>
2021-11-03 19:56:54 +0800

    report's order and json elements' order fixed by shall 1103

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a control flow graph builder for solidity, written in c++.
 
-This builder is based on EtherSolve.
+This builder is based on EtherSolve(ICPC 21).
+
+Waiting for completing ...
--- src/Utils/JsonExporter.h
+++ src/Utils/JsonExporter.h
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<json.hpp>
-#include "fifo_map.hpp"
-//#include <c++/v1/bitset>
+#include<fifo_map.hpp>
 
 using namespace std;
 using namespace nlohmann;
-using json = nlohmann::json;
-// jsoncpp使用string类型数据给Json::Value赋值有问题
-// json for modern C++的add不方便，目前想着就用遍历了
 
+// use json for modern C++
+// define template for right order
 template<class K, class V, class dummy_compare, class A>
 using my_workaround_fifo_map = fifo_map<K, V, fifo_map_compare<K>, A>;
 using my_json = basic_json<my_workaround_fifo_map>;
@@ -26,7 +24,6 @@ public:
 
     static my_json contractJsonWriter(Contract& contract) {
         my_json result;
-
         result["name"] = contract.toString();
         result["address"] = contract.getAddress();
         result["binarySource"] = contract.getBinarySource();
@@ -35,22 +32,17 @@ public:
         result["metadata"] = contract.getMetadata();
         try {
             result["solidityVersion"] = contract.getExactSolidityVersion();
-//            result.Add("solidityVersion",contract.getSolidityVersion());
         } catch (const char* e) {
             if (contract.getSolidityVersion() == SolidityVersion::UNKNOWN) {
                 result["solidityVersion"] = "unknown";
-//                result.Add("solidityVersion","unknown");
             } else {
                 result["solidityVersion"] = "<0.5.9";
-//                result.Add("solidityVersion", "<0.5.9");
             }
         }
-//        result.Add("constructorRemainingData", contract.getConstructorRemainingData());
+
         result["constructorRemainingData"] =  contract.getConstructorRemainingData();
-//        result.Add("constructorCfg", cfgJsonWriter(contract.getConstructorCfg(), result));
         result["constructorCfg"] = cfgJsonWriter(contract.getConstructorCfg());
         result["runtimeCfg"] = cfgJsonWriter(contract.getRuntimeCfg());
-//        cout<<result.dump(4)<<endl;
         return result;
     }
 
@@ -58,15 +50,11 @@ public:
 
     static my_json cfgJsonWriter(Cfg *cfg) {
         my_json result;
-
         result["bytecode"] = cfg->getBytecode().getBytes();
         result["remainingData"] = cfg->getRemainingData();
         vector<BasicBlock*> i = cfg->getBasicBlocksForIteration();
-        // 裂开，一直测试原来发现我的*m没带括号，C++丢干净了
         int count = 0;
         for (vector<BasicBlock*>::iterator m = i.begin(); m != i.end(); m++) {
-//            cout<< (*m)->getOffset()<<endl;
-//            result = nodeJsonWriter(*m, result["nodes"][count]);
             result["nodes"][count]["offset"] = (*m)->getOffset();
             result["nodes"][count]["length"] = (*m)->getLength();
             result["nodes"][count]["type"] = (*m)->getType();
@@ -74,11 +62,9 @@ public:
             result["nodes"][count]["bytecodeHex"] = (*m)->getBytes();
             result["nodes"][count]["parsedOpcodes"] = (*m)->toString();
             count++;
-//            nodeJsonWriter(*m).dump(4);
         }
         vector<my_json> result_node;
         result["successors"] = cfg->getSuccessorsMap();
-
         my_json successors = successorsWriter(cfg->getSuccessorsMap());
         result["successors"] = successors;
         result["buildReport"]["directJumpTargetErrors"] = cfg->getBuildReport().getDirectJumpTargetErrors();
@@ -93,7 +79,6 @@ public:
         result["buildReport"]["buildTimeMillis"] = cfg->getBuildReport().getBuildTimeMillis();
         return result;
     }
-
 
     static my_json successorsWriter(vector<pair<long, vector<long>>> suc) {
         my_json res;
